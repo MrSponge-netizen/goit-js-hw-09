58417de4a7bec3f46f67d49a66e112ef80f0809d
Mr-Soproonge <mrspoogeogegege@tutamail.com>
2026-06-26 01:09:47 +0300

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,10 @@ import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
+
+
 export default defineConfig(({ command }) => {
+  
   return {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
