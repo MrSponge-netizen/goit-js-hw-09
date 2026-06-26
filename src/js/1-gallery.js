4d72707c8805a2a968a4cc81457a19a6554f86ec
Mr-Soproonge <mrspoogeogegege@tutamail.com>
2026-06-27 00:58:26 +0300

    Add gallery

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,1 +1,121 @@
 console.log('Gallery');
+
+
+// 1. Correct import path for ESM
+import SimpleLightbox from "simplelightbox/dist/simple-lightbox.esm";
+
+// 2. Remember to import styles so it displays correctly
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+// 3. Initialize the constructor safely
+
+
+
+const images = [
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820__480.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/16/43/rchids-4202820_1280.jpg",
+    description: "Hokkaido Flower",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/14/22/05/container-4203677_1280.jpg",
+    description: "Container Haulage Freight",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg",
+    description: "Aerial Beach View",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2016/11/18/16/19/flowers-1835619_1280.jpg",
+    description: "Flower Blooms",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2018/09/13/10/36/mountains-3674334_1280.jpg",
+    description: "Alpine Mountains",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/23/04/landscape-4208571_1280.jpg",
+    description: "Mountain Lake Sailing",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/09/27/the-alps-4209272_1280.jpg",
+    description: "Alpine Spring Meadows",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/16/21/10/landscape-4208255_1280.jpg",
+    description: "Nature Landscape",
+  },
+  {
+    preview:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843__340.jpg",
+    original:
+      "https://cdn.pixabay.com/photo/2019/05/17/04/35/lighthouse-4208843_1280.jpg",
+    description: "Lighthouse Coast Sea",
+  },
+];
+
+const gallery = document.querySelector(".gallery");
+var docFrag = document.createDocumentFragment();
+
+
+
+images.forEach(function({preview, original, description}) {
+
+  const li  = document.createElement("li");
+  li.className = "gallery-item";
+
+    const link = document.createElement("a");
+    link.className = "gallery-link";
+    link.href = original;
+link.ariaDisabled = true;
+
+
+    var img = document.createElement('img');
+
+    img.className = "gallery-image";
+    img.src = preview;
+    img.alt = description;
+  //  img.setAttribute('datasource', original);
+
+  img.dataset.source = original;
+    
+   
+
+    link.appendChild(img);
+
+    li.appendChild(link);
+    docFrag.appendChild(li);
+});
+
+gallery.appendChild(docFrag);
+
+let gallery2 = new SimpleLightbox('.gallery a', {
+                // Optional Configuration settings
+                captionDelay: 250,    // Wait 250ms before showing caption
+                fadeSpeed: 150,       // Fade duration in ms
+                loop: true,            // Loop back to start at the end
+                captionsData : "alt"
+            });
